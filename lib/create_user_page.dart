c2db1eb5b4d9d76e98e0a3ae311c8db5dc7bdf64
micpin19 <micpin19@gmail.com>
2019-02-11 18:06:49 -0500

    Designed create user
    designed create user

--- lib/create_user_page.dart
+++ lib/create_user_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:kokushi_connect/auth.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
 class CreateUserPage extends StatefulWidget {
   CreateUserPage({this.auth});
@@ -14,24 +16,10 @@ class _CreateUserPageState extends State<CreateUserPage> {
   String _password;
   String _firstName;
   String _lastName;
-  int _month;
-  List<DropdownMenuItem> months = [
-     new DropdownMenuItem(value: 1, child: Text("January"),),
-     new DropdownMenuItem(value: 2, child: Text("Febuary"),),
-     new DropdownMenuItem(value: 3, child: Text("March"),),
-     new DropdownMenuItem(value: 4, child: Text("April"),),
-     new DropdownMenuItem(value: 5, child: Text("May"),),
-     new DropdownMenuItem(value: 6, child: Text("June"),),
-     new DropdownMenuItem(value: 7, child: Text("July"),),
-     new DropdownMenuItem(value: 8, child: Text("August"),),
-     new DropdownMenuItem(value: 9, child: Text("September"),),
-     new DropdownMenuItem(value: 10, child: Text("October"),),
-     new DropdownMenuItem(value: 11, child: Text("November"),),
-     new DropdownMenuItem(value: 12, child: Text("December"),),
-   ];
-  int _day;
-  int _year;
+  DateTime _dob;
+  bool _coach;
 
+  InputType inputType = InputType.date;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,18 +60,48 @@ class _CreateUserPageState extends State<CreateUserPage> {
         onSaved: (value) => _firstName = value,
       ),
       TextFormField(
-        decoration: InputDecoration(labelText: _month.toString()),
+        decoration: InputDecoration(labelText: "Last Name"),
         obscureText: true,
         validator: (value) => value.isEmpty? 'Last Name can\'t be empty' : null,
         onSaved: (value) => _lastName = value,
       ),
 
-      DropdownButton(
-          onChanged: (value) => _month = value,
-          items: months,
-
+      DateTimePickerFormField(
+          decoration: InputDecoration(
+              labelText: 'Date of birth'
+          ),
+          inputType: inputType,
+          format: DateFormat('yyyy-MM-dd'),
+          onSaved: (value) => _dob = value,
       ),
 
+      Column(
+        children: <Widget>[
+          RadioListTile<bool>(
+            title: const Text('Student'),
+            value: false,
+            groupValue: _coach,
+            onChanged: (value) { setState(() { _coach = value; }); },
+          ),
+          RadioListTile<bool>(
+            title: const Text('Coach'),
+            value: true,
+            groupValue: _coach,
+            onChanged: (value) { setState(() { _coach = value; }); },
+          ),
+        ],
+      ),
+      RaisedButton(
+        child: Text('Join Dojo', style: TextStyle(fontSize: 20)),
+        onPressed: null,
+      ),
+      Visibility(
+        child: RaisedButton(
+          child: Text('Create Dojo', style: TextStyle(fontSize: 20)),
+          onPressed: null,
+        ),
+        visible: _coach,
+      ),
     ];
   }
 }
