d3c3eeb72212fef08f2aeff8bbc4609a99f166f6
Max Zaremba <max.zaremba@gmail.com>
2019-02-21 09:53:29 -0500

    Updated db_control, cleaned up getters and setters

--- lib/db_control.dart
+++ lib/db_control.dart
@@ -3,84 +3,136 @@ import 'auth.dart';
 import 'dart:async';
 
 abstract class Database {
+  //user getters 
   Future<String> getFirstName(String userId);
   Future<String> getLastName(String userId);
   Future<String> getEmail(String userId);
   Future<DateTime> getDOB(String userId);
   Future<String> getRank(String userId);
+  Future<String> getDojoIdByUser(String userId);
+  Future<String> getAccountType(String userId);
+
+  //user setters
+  Future<void> setFirstName(String firstName, String userId);
+  Future<void> setLastName(String lastName, String userId);
+  Future<void> setEmail(String email, String userId);
+  Future<void> setDOB(DateTime dob, String userId);
   Future<void> setRank(String rank, String userId);
-  Future<String> getDojoId();
+  Future<void> setDojoIdForUser(String dojoId, String userId);
+  Future<void> setAccountType(String accountType, String userId);
+
+  Future<void> createAccount(String firstName, String lastName, DateTime dob, String rank, String accountType); //creates a new account
+
+  //dojo getters
   Future<String> getDojoName(String dojoId);
-  Future<void> setDojo(String name);
-  Future<bool> getAccountType();
-  Future<void> createAccount(String firstName, String lastName, DateTime dob, String rank, bool coach);
-  // first name
-  // last name
-  // email
-  // userID
-  // DOB
-  // Rank
-  // DojoID
-  // Account Type
+  Future<String> getDojoCode(String dojoId);
+  //Future<String> getDojoIdByDojo(String dojoName); //TODO
+  
+  //dojo setters
+  Future<void> setDojoCode(String code, String dojoId);
+  Future<void> setDojoName(String name, String dojoId);
 }
 
 class Db implements Database {
   final Firestore _firestore = Firestore.instance;
 
+  //gets all user information
   Future<DocumentSnapshot> userInfo(String userId) async {
     return _firestore.collection('users').document(userId).get();
   }
 
-  Future<void> createAccount(String firstName, String lastName, DateTime dob, String rank, bool coach) async {
-    return _firestore.collection("users").document().setData({ 'firstName': firstName, 'lastName': lastName, 'dob': dob, 'rank': rank, 'coach': coach });
-  }
-
-  Future<DateTime> getDOB(String userId) async {
-    DocumentSnapshot document = await userInfo(userId);
-    return document.data['dob'];
-  }
-
-  Future<String> getDojoId() async {
-    // TODO: implement getDojoId
-    return null;
-  }
-
-  Future<String> getDojoName([String dojoId]) async {
-    // TODO: implement getDojoName
-    return null;
-  }
-
-  Future<String> getEmail(String userId) async {
-    // TODO: implement getEmail
-    return null;
-  }
-
+  //getters for user
   Future<String> getFirstName(String userId) async {
     DocumentSnapshot document = await userInfo(userId);
     return document.data['firstName'];
   }
 
   Future<String> getLastName(String userId) async {
-    // TODO: implement getLastName
-    return null;
+    DocumentSnapshot document = await userInfo(userId);
+    return document.data['lastName'];
+  }
+
+  Future<String> getEmail(String userId) async {
+    DocumentSnapshot document = await userInfo(userId);
+    return document.data['email'];
+  }
+  
+  Future<DateTime> getDOB(String userId) async {
+    DocumentSnapshot document = await userInfo(userId);
+    return document.data['dob'];
   }
 
   Future<String> getRank(String userId) async {
-    // TODO: implement getRank
-    return null;
+    DocumentSnapshot document = await userInfo(userId);
+    return document.data['rank'];
   }
 
-  Future<String> setDojo(String name) async {
-    // TODO: implement setDojo
-    return null;
+  Future<String> getDojoIdByUser(String userId) async {
+    DocumentSnapshot document = await userInfo(userId);
+    return document.data['dojoId'];
   }
 
-  Future<String> setRank(String rank, String userId) async {
-    // TODO: implement setRank
-    return null;
+  Future<String> getAccountType(String userId) async {
+    DocumentSnapshot document = await userInfo(userId);
+    return document.data['accountType'];
   }
 
-  Future<bool> getAccountType() async {
-    return null;
+  //setters for users
+  Future<void> setFirstName(String firstName, String userId) async {
+    return _firestore.collection("users").document(userId).setData({ 'firstName': firstName});
+  }
+
+  Future<void> setLastName(String lastName, String userId) async {
+    return _firestore.collection("users").document(userId).setData({ 'lastName': lastName});
+  }
+
+  Future<void> setEmail(String email, String userId) async {
+    return _firestore.collection("users").document(userId).setData({ 'email': email});
+  }
+
+  Future<void> setDOB(DateTime dob, String userId) async{
+    return _firestore.collection("users").document(userId).setData({ 'dob': dob});
+  }
+
+  Future<void> setRank(String rank, String userId) async {
+    return _firestore.collection("users").document(userId).setData({ 'rank': rank});
+  }
+
+  Future<void> setDojoIdForUser(String dojoId, String userId) async {
+    return _firestore.collection("users").document(userId).setData({ 'dojoId': dojoId});
+  }
+
+  Future<void> setAccountType(String accountType, String userId){
+    return _firestore.collection("users").document(userId).setData({ 'accountType': accountType});
+  }
+
+  //account creation
+  Future<void> createAccount(String firstName, String lastName, DateTime dob, String rank, String accountType) async {
+    //TODO: implement with setters
+    return _firestore.collection("users").document().setData({ 'firstName': firstName, 'lastName': lastName, 'dob': dob, 'rank': rank, 'accountType': accountType });
+  }
+
+  //gets all dojo information
+  Future<DocumentSnapshot> dojoInfo(String dojoId) async {
+    return _firestore.collection('dojos').document(dojoId).get();
+  }
+
+  //getters for dojo
+  Future<String> getDojoName([String dojoId]) async {
+    DocumentSnapshot document = await userInfo(dojoId);
+    return document.data['dojoName'];
+  }
+
+  Future<String> getDojoCode(String dojoId) async {
+    DocumentSnapshot document = await userInfo(dojoId);
+    return document.data['dojoCode'];
+  }
+
+  //setters for dojo
+  Future<void> setDojoCode(String code, String dojoId) async {
+    return _firestore.collection("dojos").document(dojoId).setData({ 'dojoCode': code});
+  }
+  Future<void> setDojoName(String name, String dojoId) async {
+    return _firestore.collection("dojos").document(dojoId).setData({ 'dojoName': name});
   }
 }
