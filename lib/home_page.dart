04d06c93842f77bcf92179431dc2d6369aba6d81
Evan Demoleas <evan@Evans-MacBook-Pro.local>
2019-03-01 14:18:47 -0500

    Revert "Rework of sign in and sign out + auto routes to dojo if null"
    This reverts commit d91308303a8cb5a7e3f5c392adc0160f9e5f38d7.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
-import 'custom_app_bar.dart';
-import 'root_page.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({this.auth});
+  HomePage({this.auth, this.onSignedOut});
   final BaseAuth auth;
+  final VoidCallback onSignedOut;
+
+  void _signOut() async {
+    try {
+      await auth.signOut();
+      onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
+      appBar: AppBar(
         title: Text('Welcome'),
-        context: context,
-        auth: auth,
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Logout', style: TextStyle(fontSize: 17.0, color: Colors.white)),
+            onPressed: _signOut,
+          )
+        ],
       ),
       body: Container(
         child: Center(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
 import 'root_page.dart';
-import 'db_control.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RootPage(auth: Auth(), db: Db()),
+      home: RootPage(auth: Auth()),
     );
   }
 }
